bc9f88103488b04966cf49345860ddd118c182ca
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2026-04-29 11:19:44 +1000

    feat: AWS input validation compliance (#171)

--- spinifex/gateway/elbv2_test.go
+++ spinifex/gateway/elbv2_test.go
@@ -1,10 +1,39 @@
 package gateway
 
 import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/mulgadc/spinifex/spinifex/awserrors"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func setupELBv2Request(body string) *http.Request {
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(body))
+	ctx := context.WithValue(req.Context(), ctxService, "elasticloadbalancing")
+	ctx = context.WithValue(ctx, ctxAccountID, "123456789012")
+	return req.WithContext(ctx)
+}
+
+func TestELBv2Request_MissingAction(t *testing.T) {
+	gw := &GatewayConfig{DisableLogging: true}
+	w := httptest.NewRecorder()
+	err := gw.ELBv2_Request(w, setupELBv2Request(""))
+	require.Error(t, err)
+	assert.Equal(t, awserrors.ErrorMissingAction, err.Error())
+}
+
+func TestELBv2Request_UnknownAction(t *testing.T) {
+	gw := &GatewayConfig{DisableLogging: true}
+	w := httptest.NewRecorder()
+	err := gw.ELBv2_Request(w, setupELBv2Request("Action=FakeAction"))
+	require.Error(t, err)
+	assert.Equal(t, awserrors.ErrorInvalidAction, err.Error())
+}
 
 func TestELBv2ActionsMap_AllActionsRegistered(t *testing.T) {
 	expectedActions := []string{
